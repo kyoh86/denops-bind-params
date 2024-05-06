08d5b133519a92705531ef3e7867c34a085987e5
kyoh86 <me@kyoh86.dev>
2024-05-06 15:20:38 +0900

    add test

---
+++ denops/bind-params/bind_test.ts
@@ -0,0 +1,128 @@
+import { test } from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.223.0/assert/mod.ts";
+import { bindDispatcher } from "./bind.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
+
+test({
+  mode: "all",
+  name: "It can call all bound methods",
+  fn: async () => {
+    const dispatcher = bindDispatcher({
+      hello: (...args: unknown[]) => {
+        const { name } = ensure(args[0], is.ObjectOf({ name: is.String }));
+        return `Hello, ${name}!`;
+      },
+      bye: (...args: unknown[]) => {
+        const { name } = ensure(args[0], is.ObjectOf({ name: is.String }));
+        return `Bye, ${name}!`;
+      },
+    });
+    assertEquals(await dispatcher.hello({ name: "Alice" }), "Hello, Alice!");
+    assertEquals(await dispatcher.bye({ name: "Alice" }), "Bye, Alice!");
+  },
+});
+
+test({
+  mode: "all",
+  name: "It can call a bound method with a reserved parameter",
+  fn: async () => {
+    const dispatcher = bindDispatcher({
+      hello: (...args: unknown[]) => {
+        const { name } = ensure(args[0], is.ObjectOf({ name: is.String }));
+        return `Hello, ${name}!`;
+      },
+    });
+    await dispatcher["params:set-one"]("hello", "name", "Alice");
+    assertEquals(await dispatcher.hello(), "Hello, Alice!");
+  },
+});
+
+test({
+  mode: "all",
+  name:
+    "It can call a bound method with a reserved parameter and a normal parameter",
+  fn: async () => {
+    const dispatcher = bindDispatcher({
+      hello: (...args: unknown[]) => {
+        const { name, age } = ensure(
+          args[0],
+          is.ObjectOf({ name: is.String, age: is.Number }),
+        );
+        return `Hello, ${name}! You are ${age} years old.`;
+      },
+    });
+    await dispatcher["params:set-one"]("hello", "name", "Alice");
+    assertEquals(
+      await dispatcher.hello({ age: 20 }),
+      "Hello, Alice! You are 20 years old.",
+    );
+  },
+});
+
+test({
+  mode: "all",
+  name:
+    "It can call a bound method with a reserved parameter and override it with a normal parameter",
+  fn: async () => {
+    const dispatcher = bindDispatcher({
+      hello: (...args: unknown[]) => {
+        const { name } = ensure(args[0], is.ObjectOf({ name: is.String }));
+        return `Hello, ${name}!`;
+      },
+    });
+    await dispatcher["params:set-one"]("hello", "name", "Alice");
+    assertEquals(await dispatcher.hello({ name: "Bob" }), "Hello, Bob!");
+  },
+});
+
+test({
+  mode: "all",
+  name:
+    "It can call a bound method with a reserved parameter and override it with a normal parameter",
+  fn: async () => {
+    const dispatcher = bindDispatcher({
+      hello: (...args: unknown[]) => {
+        const { name } = ensure(args[0], is.ObjectOf({ name: is.String }));
+        return `Hello, ${name}!`;
+      },
+    });
+    await dispatcher["params:set-one"]("hello", "name", "Alice");
+    assertEquals(await dispatcher.hello({ name: "Bob" }), "Hello, Bob!");
+  },
+});
+
+test({
+  mode: "all",
+  name:
+    "It can call a bound method with a reserved default value for across methods and reserved parameter and override it with a normal parameter",
+  fn: async () => {
+    const dispatcher = bindDispatcher({
+      hello: (...args: unknown[]) => {
+        const { name, age } = ensure(
+          args[0],
+          is.ObjectOf({ name: is.String, age: is.Number }),
+        );
+        return `Hello, ${name}! You are ${age} years old.`;
+      },
+      bye: (...args: unknown[]) => {
+        const { name } = ensure(args[0], is.ObjectOf({ name: is.String }));
+        return `Bye, ${name}!`;
+      },
+    });
+    await dispatcher["params:set-one"]("_", "name", "Alice");
+    await dispatcher["params:set-one"]("bye", "name", "Bob");
+    assertEquals(
+      await dispatcher.hello({ age: 20 }),
+      "Hello, Alice! You are 20 years old.",
+    );
+    assertEquals(await dispatcher.bye(), "Bye, Bob!");
+    assertEquals(
+      await dispatcher.hello({ name: "Charlie", age: 30 }),
+      "Hello, Charlie! You are 30 years old.",
+    );
+    assertEquals(
+      await dispatcher.bye({ name: "Charlie" }),
+      "Bye, Charlie!",
+    );
+  },
+});
---
+++ denops/bind-params/store_test.ts
@@ -0,0 +1,87 @@
+import { ParamStore } from "./store.ts";
+import { test } from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.223.0/assert/mod.ts";
+
+test({
+  mode: "all",
+  name: "stores a parameter value for a specific method",
+  fn: () => {
+    const paramStore = new ParamStore();
+    paramStore.storeParam("method1", "param1", true);
+    assertEquals(paramStore.getParamsForMethod("method1"), { param1: true });
+  },
+});
+
+test({
+  mode: "all",
+  name: "stores a default parameter value for all methods",
+  fn: () => {
+    const paramStore = new ParamStore();
+    paramStore.storeParam("_", "defaultParam", false);
+    assertEquals(paramStore.getParamsForMethod("_"), { defaultParam: false });
+    assertEquals(paramStore.getParamsForMethod("method1"), {
+      defaultParam: false,
+    });
+  },
+});
+
+test({
+  mode: "all",
+  name: "stores parameters for multiple methods",
+  fn: () => {
+    const paramStore = new ParamStore();
+    paramStore.storeParams({
+      method1: { param1: true, param2: false },
+      method2: { param3: true, param4: false },
+    });
+    assertEquals(paramStore.getParamsForMethod("method1"), {
+      param1: true,
+      param2: false,
+    });
+    assertEquals(paramStore.getParamsForMethod("method2"), {
+      param3: true,
+      param4: false,
+    });
+  },
+});
+
+test({
+  mode: "all",
+  name: "gets all stored parameter values",
+  fn: () => {
+    const paramStore = new ParamStore();
+    paramStore.storeParam("_", "defaultParam1", true);
+    paramStore.storeParam("method1", "param1", false);
+    assertEquals(paramStore.getAllParams(), {
+      _: { defaultParam1: true },
+      method1: { param1: false },
+    });
+  },
+});
+
+test({
+  mode: "all",
+  name: "clears a single parameter value for a specific method or all methods",
+  fn: () => {
+    const paramStore = new ParamStore();
+    paramStore.storeParam("_", "defaultParam1", true);
+    paramStore.clearParam("_", "defaultParam1");
+    assertEquals(paramStore.getParamsForMethod("_"), {});
+
+    paramStore.storeParam("method1", "param1", false);
+    paramStore.clearParam("method1", "param1");
+    assertEquals(paramStore.getParamsForMethod("method1"), {});
+  },
+});
+
+test({
+  mode: "all",
+  name: "clears all parameter values for a specific method or all methods",
+  fn: () => {
+    const paramStore = new ParamStore();
+    paramStore.storeParam("_", "defaultParam1", true);
+    paramStore.storeParam("method1", "param1", false);
+    paramStore.clearAllParams();
+    assertEquals(paramStore.getAllParams(), {});
+  },
+});
